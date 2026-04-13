603809713c4eab94b4ba2996ae5b47ef709c7e50
tiendinhne <dinhtien125@gmail.com>
2026-04-13 15:38:06 +0700

    fix entrypoint

--- web-frontend-server/entrypoint.sh
+++ web-frontend-server/entrypoint.sh
@@ -8,6 +8,6 @@ else
 fi
 
 # Match <body ...> với bất kỳ attributes nào
-sed -i "s|<body\([^>]*\)>|<body\1><div style='${COLOR};color:#fff;text-align:center;padding:8px;font-family:sans-serif;font-weight:bold;position:fixed;top:0;width:100%;z-index:9999;'>🌐 Web Frontend Server — Instance #${INSTANCE}</div>|" /usr/share/nginx/html/index.html
+sed -i "s|<body\([^>]*\)>|<body\1><div style='${COLOR};color:#fff;text-align:center;padding:8px;font-family:sans-serif;font-weight:bold;position:fixed;top:0;width:100%;z-index:0;'>🌐 Web Frontend Server — Instance #${INSTANCE}</div>|" /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"
